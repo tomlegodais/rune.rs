be99da47154fba905d1f75e66259ec91efe0982b
Tom LeGodais <tomlegodais@gmail.com>
2026-03-20 18:17:57 -0400

    chore: remove un-needed keyword

--- game/src/player/mod.rs
+++ game/src/player/mod.rs
@@ -1,17 +1,17 @@
 mod appearance;
-pub(crate) mod gpi;
+mod gpi;
 mod mask;
 mod player;
 mod skill;
-mod viewport;
 mod ui;
+mod viewport;
 mod widget;
 
 pub(crate) use appearance::{Appearance, AppearanceEncoder};
 pub(crate) use mask::{MaskBlock, MaskEncoder, MaskFlags, MoveTypeMask};
 pub(crate) use player::{Player, PlayerSnapshot};
-pub(crate) use viewport::Viewport;
 pub(crate) use skill::{Skill, SkillManager};
+pub(crate) use viewport::Viewport;
 pub(crate) use widget::{
     ChatboxWidget, InventoryWidget, RootWidget, ScreenWidget, Widget, WidgetManager,
 };
