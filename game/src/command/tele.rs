d889839499c0b0ac00ea74086bfcbc8f47031b0d
Tom LeGodais <tomlegodais@gmail.com>
2026-03-23 04:23:45 -0400

    fix: make plane optional in tele command, default to 0

--- game/src/command/tele.rs
+++ game/src/command/tele.rs
@@ -5,7 +5,8 @@ use crate::world::Position;
 use macros::command;
 
 #[command(name = "tele")]
-async fn handle(player: &mut Player, x: i32, y: i32, plane: i32) {
+async fn handle(player: &mut Player, x: i32, y: i32, plane: Option<i32>) {
+    let plane = plane.unwrap_or(0);
     let dest = Position::new(x, y, plane);
     player.teleport(dest);
     send_message!(player, "Teleporting to {}, {}, {}", x, y, plane);
