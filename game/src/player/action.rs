48ed9df2ec40bbb9777677cc2a4a063376f82527
Tom LeGodais <tomlegodais@gmail.com>
2026-03-26 03:38:57 -0400

    feat: add SkillActionBuilder with builder pattern and IntoFuture

--- game/src/player/action.rs
+++ game/src/player/action.rs
@@ -110,6 +110,66 @@ pub fn unlock(shared: &ActionShared) {
     shared.locked.store(false, Ordering::Relaxed);
 }
 
+pub struct SkillActionBuilder {
+    shared: Arc<ActionShared>,
+    animation: Option<u16>,
+    interval: u16,
+    success_predicate: Box<dyn Fn(&Player) -> bool + Send>,
+    success_handler: Box<dyn FnMut(&mut Player) + Send>,
+}
+
+impl SkillActionBuilder {
+    pub fn new(shared: Arc<ActionShared>) -> Self {
+        Self {
+            shared,
+            animation: None,
+            interval: 4,
+            success_predicate: Box::new(|_| false),
+            success_handler: Box::new(|_| {}),
+        }
+    }
+
+    pub fn animation(mut self, id: u16) -> Self {
+        self.animation = Some(id);
+        self
+    }
+
+    pub fn interval(mut self, ticks: u16) -> Self {
+        self.interval = ticks;
+        self
+    }
+
+    pub fn on_success(
+        mut self,
+        predicate: impl Fn(&Player) -> bool + Send + 'static,
+        handler: impl FnMut(&mut Player) + Send + 'static,
+    ) -> Self {
+        self.success_predicate = Box::new(predicate);
+        self.success_handler = Box::new(handler);
+        self
+    }
+}
+
+impl std::future::IntoFuture for SkillActionBuilder {
+    type Output = ();
+    type IntoFuture = Pin<Box<dyn Future<Output = ()> + Send>>;
+
+    fn into_future(self) -> Self::IntoFuture {
+        Box::pin(async move {
+            let mut predicate = self.success_predicate;
+            let mut handler = self.success_handler;
+            loop {
+                delay(&self.shared, self.interval).await;
+                let player = active_player();
+                if predicate(player) {
+                    handler(player);
+                    break;
+                }
+            }
+        })
+    }
+}
+
 pub fn is_action_locked(player: &Player) -> bool {
     let world = player.world();
     world
