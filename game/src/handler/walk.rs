1bcf5f75a79617047c17f38140f8aa121251c783
Tom LeGodais <tomlegodais@gmail.com>
2026-03-26 02:57:13 -0400

    feat: walk handler checks action lock and cancels active actions

--- game/src/handler/walk.rs
+++ game/src/handler/walk.rs
@@ -1,5 +1,5 @@
 use super::MessageHandler;
-use crate::player::{Interaction, Player};
+use crate::player::{Interaction, Player, is_action_locked};
 use crate::with_movement;
 use crate::world::Position;
 use macros::message_handler;
@@ -7,6 +7,11 @@ use net::WalkRequest;
 
 #[message_handler]
 async fn handle(player: &mut Player, msg: WalkRequest) {
+    if is_action_locked(player) {
+        return;
+    }
+
+    player.world().action_states.lock().remove(&player.index);
     player.systems.get_mut::<Interaction>().clear();
     let dest = Position::new(msg.x as i32, msg.y as i32, player.position.plane);
     with_movement!(player, |m, ctx| m
